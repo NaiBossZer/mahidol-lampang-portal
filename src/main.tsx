6ac24d51f47d4421eb7142e6ba070e45d9557059
NaiBossZer <apiwat.suw@mahidol.ac.th>
2026-09-10 19:35:59 +0700

    feat(i18n): provide language state to portal

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 ﻿import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { LanguageProvider } from "./i18n/LanguageContext";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </BrowserRouter>,
 );
